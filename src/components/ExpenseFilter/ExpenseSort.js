e334019cc2f12ab01bd6ae054bd6d75d5759ab3c
dnomokonov <zmnomokon715@gmail.com>
2024-09-29 20:05:42 +0700

    start filters

---
+++ src/components/ExpenseFilter/ExpenseSort.js
@@ -0,0 +1,21 @@
+export default function ExpenseSort({sort, setSort, order, setOrder}) {
+    const sortChange = (event) => {
+        const value = event.target.value;
+        const [newSort, newOrder] = value.split('-')
+        
+        setSort(newSort)
+        setOrder(newOrder);
+    }
+    
+    return (
+        <div className="sort-block">
+            <label>Сортировать по:</label>
+            <select value={`${sort}-${order}`} onChange={sortChange}>
+                <option value="date-desc">Дата | Убывание</option>
+                <option value="date-asc">Дата | Возрастание</option>
+                <option value="amount-desc">Сумма | Убывание</option>
+                <option value="amount-asc">Сумма | Возрастание</option>
+            </select>
+        </div>
+    )
+}
